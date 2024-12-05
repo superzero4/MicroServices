3dca2dfce3d57779546dc1daa13f91c4eb4efee7
superzero4 <78915984+superzero4@users.noreply.github.com>
2024-12-05 13:17:17 -0500

    Helper script

---
+++ Build/RunAll.ps1
@@ -0,0 +1,22 @@
+﻿# Get the root directory of the project
+$rootDir = Get-Location
+
+# Define the build configurations to check
+$config = "Release"
+#$config = "Debug"
+
+# Get all project directories
+$projectDirs = @("MathProcessor","TextProcessor","CommandValidator","CommandSource","CommandMonitor")
+#Get-ChildItem -Path $rootDir -Directory
+foreach ($projectDir in $projectDirs) {
+        $exePath = Join-Path -Path $rootDir -ChildPath $projectDir
+        $exePath = Join-Path $exePath -ChildPath "bin\$config\net8.0\$($projectDir).exe"
+        if (Test-Path $exePath) {
+            Write-Host "Running $exePath"
+            Start-Process -FilePath $exePath
+            #-NoNewWindow -Wait
+        }else{
+            Write-Host "Could not find $exePath"
+        }
+}
+Start-Sleep -Seconds 30
